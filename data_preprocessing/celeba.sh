afdd3f25e6bcfd1e0213c581ef969a23990cd039
Tianyu Wang <1463026393@qq.com>
2020-02-12 15:30:27 +0000

    add mask test split

--- data_preprocessing/celeba.sh
+++ data_preprocessing/celeba.sh
@@ -9,3 +9,9 @@ mkdir datasets
 python ./scripts/flist.py --path ./img_align_celeba/img_align_celeba --output ./datasets/celeba_train.flist
 python ./scripts/flist.py --path ./data/celeba/test --output ./datasets/celeba_test.flist
 python ./scripts/flist.py --path ./data/celeba/eval --output ./datasets/celeba_eval.flist
+
+mkdir data/mask
+mkdir data/mask/train
+mkdir data/mask/test
+mkdir data/mask/eval
+python ./scripts/flist.py --path ./mask/testing_mask_dataset --output ./datasets/mask_test.flist
