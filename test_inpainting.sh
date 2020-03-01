b260e51240e93b161fb307a33428c60f632127e8
Ming Jin <s1655829@uhtred.inf.ed.ac.uk>
2020-03-01 21:55:11 +0000

    fix masks while testing

--- test_inpainting.sh
+++ test_inpainting.sh
@@ -6,4 +6,4 @@ conda activate edge
 
 python test.py \
   --model 3 \
-  --checkpoints ./checkpoints/celeba_inpainting --input ../edge-connect/data/celeba/test --output ./checkpoints/celeba_inpainting/results_celeba_dummy
+  --checkpoints ./checkpoints/celeba_inpainting --input ../edge-connect/data/celeba/test --output ./checkpoints/celeba_inpainting/results
