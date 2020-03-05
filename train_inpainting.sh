2e70b60d7c140c319a463c1413728c98da2743f6
XianmingJin <s1655829@gmail.com>
2020-03-05 23:50:20 +0000

    hahahah

--- train_inpainting.sh
+++ train_inpainting.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo ". /home/s1655829/miniconda2/etc/profile.d/conda.sh" >> ~/.bashrc
-echo ". /home/s1655829/miniconda2/etc/profile.d/conda.sh" >> ~/.benv
-source ~/.benv
-conda activate edge
 
-python train.py --model 2 --checkpoints ./checkpoints/celeba_inpainting
+python train.py --model 2 --checkpoints ./checkpoints/celeba_inpaint
